9da1f4550d5d665a6d9e4c16a8efef1fa23fce41
xf <14182909+xh_13_0@user.noreply.gitee.com>
2025-03-20 20:49:36 +0800

    pinia以及tailwind的安装

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,10 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import { router } from './router'
+import { createPinia } from 'pinia'
 
 const app = createApp(App)
+const Pinia = createPinia()
 app.use(router)
+app.use(Pinia)
 app.mount('#app')
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,9 +3,25 @@
 </script>
 
 <template>
- 111
+ <div class=" bg-amber-100 flex flex-col justify-center items-center h-screen">
+    <div class=" flex">
+      <div class="m-[30px] "><img src="../assets/vue.svg" class=" size-[100px]"></div>
+      <div class="m-[30px] bg-amber-600 rounded-full size-[100px] avatar-bg"></div>
+    </div>
+   <div>
+      <text class="text-[50px]">vue+ts+vite</text>
+   </div>
+   <div>
+      <text class="text-[20px]">一个小型的vue+ts+vite模板，内含tailwind，pinia及其持久化插件</text>
+   </div>
+ </div>
+ 
 </template>
 
 <style scoped>
-
+.avatar-bg {
+  background-image: url('../assets/avatar.jpg');
+  background-size: cover;
+  background-position: center;
+}
 </style>
